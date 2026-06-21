6e328233ffd6efa45ad76a826f649d71b739dcd7
Justinhere2025 <scholtenj@wsc.school.nz>
2026-06-22 11:54:03 +1200

    Update mian.py

--- Alternate main.py/mian.py
+++ Alternate main.py/mian.py
@@ -1,10 +1,21 @@
-shopping_list = ['apples','plums','pizza']
-print(shopping_list[2])
+# shopping_list = ['apples','plums','pizza']
+# print(shopping_list[2])
 
-shopping_list = ['apples','plums','pizza']
-print(shopping_list[2])
+# shopping_list = ['apples','plums','pizza']
+# print(shopping_list[2])
 
 
-import random
-shopping_list = ['apples','plums','pizza']
-print(shopping_list[random.randint(0,2)])
+# import random
+# shopping_list = ['apples','plums','pizza']
+# print(shopping_list[random.randint(0,2)])
+
+def get_number():
+    while True:
+        num = input("Give me a number.")
+        try:
+            num = int(num)
+            return
+        except:
+            print("That's not a number")
+
+num = get_number()
